6dd006b035dfc005f475cd27cb3081435fe37253
Spatay2000 <Spatay.148@gmail.com>
2021-04-26 07:08:59 +0600

    Java EE endterm project and all assignmnet

--- src/main/java/dl/iitu/kz/LoginServlet.java
+++ src/main/java/dl/iitu/kz/LoginServlet.java
@@ -1,42 +1,106 @@
 package dl.iitu.kz;
-
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-public class LoginServlet  extends HttpServlet {
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+
+@WebServlet(urlPatterns = { "/login" })
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public LoginServlet() {
+        super();
+    }
+
+    // Show Login page.
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-         PrintWriter out = response.getWriter(); {
 
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
+        // Forward to /WEB-INF/views/loginView.jsp
+        // (Users can not access directly into JSP pages placed in WEB-INF)
+        RequestDispatcher dispatcher //
+                = this.getServletContext().getRequestDispatcher("/loginView.jsp");
 
+        dispatcher.forward(request, response);
 
-            String logemail = request.getParameter("email");
-            String logpass = request.getParameter("password");
+    }
 
-            UserDatabase db =  new UserDatabase(ConnectionPro.getConnection());
-            User user = db.logUser(logemail, logpass);
+    // When the user enters userName & password, and click Submit.
+    // This method will be executed.
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String userName = request.getParameter("userName");
+        String password = request.getParameter("password");
+        String rememberMeStr = request.getParameter("rememberMe");
+        boolean remember = "Y".equals(rememberMeStr);
 
-            if(user!=null){
-                HttpSession session = request.getSession();
-                session.setAttribute("logUser", user);
-                response.sendRedirect("welcome.jsp");
-            }else{
-                out.println("user not found");
+        UserAccount user = null;
+        boolean hasError = false;
+        String errorString = null;
+
+        if (userName == null || password == null || userName.length() == 0 || password.length() == 0) {
+            hasError = true;
+            errorString = "Required username and password!";
+        } else {
+            Connection conn = MyUtils.getStoredConnection(request);
+            try {
+                // Find the user in the DB.
+                user = DBUtils.findUser(conn, userName, password);
+
+                if (user == null) {
+                    hasError = true;
+                    errorString = "User Name or password invalid";
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                hasError = true;
+                errorString = e.getMessage();
+            }
+        }
+        // If error, forward to /WEB-INF/views/login.jsp
+        if (hasError) {
+            user = new UserAccount();
+            user.setUserName(userName);
+            user.setPassword(password);
+
+            // Store information in request attribute, before forward.
+            request.setAttribute("errorString", errorString);
+            request.setAttribute("user", user);
+
+            // Forward to /WEB-INF/views/login.jsp
+            RequestDispatcher dispatcher //
+                    = this.getServletContext().getRequestDispatcher("/loginView.jsp");
+
+            dispatcher.forward(request, response);
+        }
+        // If no error
+        // Store user information in Session
+        // And redirect to userInfo page.
+        else {
+            HttpSession session = request.getSession();
+            MyUtils.storeLoginedUser(session, user);
+
+            // If user checked "Remember me".
+            if (remember) {
+                MyUtils.storeUserCookie(response, user);
+            }
+            // Else delete cookie.
+            else {
+                MyUtils.deleteUserCookie(response);
             }
 
-            out.println("</body>");
-            out.println("</html>");
+            // Redirect to userInfo page.
+            response.sendRedirect(request.getContextPath() + "/userInfo");
         }
     }
+
 }
